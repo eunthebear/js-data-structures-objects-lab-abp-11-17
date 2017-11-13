d0a707a85921932596a7cd53159b89a82b0115c9
eunthebear <eun.david@gmail.com>
2017-11-13 05:37:19 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,3 +4,8 @@ const driver = {};
 function updateDriverWithKeyAndValue(obj, key, value) {
   return Object.assign({}, obj, {[key]: value});
 }
+
+function destructivelyUpdateDriverWithKeyAndValue(obj, key, value) {
+  obj[key] = value;
+  return obj;
+}
